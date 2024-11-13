c697d23b2d6ec83758eb65e7ccf2673f47f61495
jmayta1984 <jmayta@pucp.edu.pe>
2024-11-13 13:53:17 -0500

    feat (episodes: show characters for each episode

--- RickAndMortyApp/RickAndMortyApp/RickAndMortyAppApp.swift
+++ RickAndMortyApp/RickAndMortyApp/RickAndMortyAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RickAndMortyAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
--- RickAndMortyApp/RickAndMortyApp/Services/EpisodeService.swift
+++ RickAndMortyApp/RickAndMortyApp/Services/EpisodeService.swift
@@ -21,20 +21,33 @@ class EpisodeService {
                     let episodesDto = episodesResponse.results
                     var episodes: [Episode] = []
                     
+                    let mainDispatchGroup = DispatchGroup()
+                    
+                    
                     episodesDto.forEach{ episodeDto in
+                        mainDispatchGroup.enter()
+
+                        let dispatchGroup = DispatchGroup()
                         var characters: [Character] = []
                         episodeDto.characters.forEach { url in
+                            dispatchGroup.enter()
                             CharacterService().getCharacterById(url: url) { character, message in
                                 guard let character = character else { return  }
                                 characters.append(character)
-
+                                dispatchGroup.leave()
                             }
                         }
+                        dispatchGroup.notify(queue: .main) {
+                            let episode = Episode(id: episodeDto.id, name: episodeDto.name, airDate: episodeDto.airDate, description: episodeDto.episode, characters: characters )
+                            episodes.append(episode)
+                            mainDispatchGroup.leave()
 
-                        let episode = Episode(id: episodeDto.id, name: episodeDto.name, airDate: episodeDto.airDate, description: episodeDto.episode, characters: characters )
-                        episodes.append(episode)
+                        }
+                        
                     }
-                    completion(episodes, nil)
+                    mainDispatchGroup.notify(queue: .main) {
+                        completion(episodes, nil)
+                    }
                     
                 } catch let error {
                     completion(nil, "Error: \(error.localizedDescription)")
