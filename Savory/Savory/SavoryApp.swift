a08da9cbd4a444f783a99f294c66f45993ab9e34
Jorge M <38768001+jmayta1984@users.noreply.github.com>
2024-11-27 11:36:06 -0500

    chore: initial project setup

---
+++ Savory/Savory/SavoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavoryApp.swift
+//  Savory
+//
+//  Created by Alumno on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SavoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
