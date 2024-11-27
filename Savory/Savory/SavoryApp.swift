13482b9772cdd55babcbfa96a6fe7ada3e2f5163
Jorge M <38768001+jmayta1984@users.noreply.github.com>
2024-11-27 12:20:50 -0500

    chore: initial project setup

---
+++ Savory/Savory/SavoryApp.swift
@@ -0,0 +1,18 @@
+//
+//  SavoryApp.swift
+//  Savory
+//
+//  Created by Alumno on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SavoryApp: App {
+    private let googleMapManager = GoogleMapManager()
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
